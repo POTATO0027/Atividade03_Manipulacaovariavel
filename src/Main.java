e52dd19d2f025dc45718b95f755400497b6718ae
otaviobleich <ferreiraotavio577@gmail.com>
2024-08-19 21:51:43 -0300

    qualquer coisa

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.text.DecimalFormat;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Criação de Variáveis
+        String nomeProduto = "Notebook";
+        double precoUnitario = 5500.00;
+        int quantidadeComprada = 8;
+        double taxaImposto = 0.82; // 15%
+        double margemLucro = 0.45; // 20%
+
+        // Operações Simples
+        double valorTotalSemImpostos = precoUnitario * quantidadeComprada;
+        double valorImposto = valorTotalSemImpostos * taxaImposto;
+        double valorTotalComImpostos = valorTotalSemImpostos + valorImposto;
+        double precoVendaSugerido = valorTotalComImpostos * (1 + margemLucro);
+
+        // Arredondando e formatando os valores
+        DecimalFormat df = new DecimalFormat("R$ #,##0.00");
+        String valorTotalComImpostosFormatado = df.format(valorTotalComImpostos);
+        String precoVendaSugeridoFormatado = df.format(precoVendaSugerido);
+
+        // Mensagem final
+        String mensagemFinal = "Produto: " + nomeProduto + "\n" +
+                "Valor total com impostos: " + valorTotalComImpostosFormatado + "\n" +
+                "Preço de venda sugerido: " + precoVendaSugeridoFormatado;
+
+        // Exibição de Resultados
+        System.out.println("Nome do Produto: " + nomeProduto);
+        System.out.println("Preço Unitário: R$ " + precoUnitario);
+        System.out.println("Quantidade Comprada: " + quantidadeComprada);
+        System.out.println("Taxa de Imposto: " + (taxaImposto * 100) + "%");
+        System.out.println("Margem de Lucro: " + (margemLucro * 100) + "%");
+        System.out.println("Valor Total Sem Impostos: R$ " + valorTotalSemImpostos);
+        System.out.println("Valor do Imposto: R$ " + valorImposto);
+        System.out.println("Valor Total Com Impostos: " + valorTotalComImpostosFormatado);
+        System.out.println("Preço de Venda Sugerido: " + precoVendaSugeridoFormatado);
+        System.out.println();
+        System.out.println(mensagemFinal);
     }
 }
